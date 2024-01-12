b0ca308394e3f1eacc2f4221d5093e4fbed924a0
hossain-asif <minhazhossainasif@gmail.com>
2024-01-12 18:34:05 +0600

    1

---
+++ module-7/main.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+
+
+// "use strict";
+// myName();
+// function myName(){
+//     name = "Asif";
+// }
+
+// spread operator
+// let arr1=[1,2,3,4,5];
+// let arr2=[...arr1,10,11,12,1,31,4];
+// console.log(arr2);
+
+
+// without spread operator
+let arr1=[1,2,3,4,5];
+let arr2=[10,11,12,1,31,4];
+arr2.push(arr1);
+console.log(arr2);

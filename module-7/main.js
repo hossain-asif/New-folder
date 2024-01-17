dea5c7de94e4dec453f2d6747908f103bd637ee6
hossain-asif <minhazhossainasif@gmail.com>
2024-01-17 19:21:04 +0600

    weather done

--- module-7/main.js
+++ module-7/main.js
@@ -14,7 +14,30 @@
 
 
 // without spread operator
-let arr1=[1,2,3,4,5];
-let arr2=[10,11,12,1,31,4];
-arr2.push(arr1);
-console.log(arr2);
+// let arr1=[1,2,3,4,5];
+// let arr2=[10,11,12,1,31,4];
+// arr2.push(arr1);
+// console.log(arr2);
+
+//rest operator
+// function func(...nums){
+//     console.log(nums);
+// }
+
+// func(1,2,3,4,5,6,7);
+
+
+// let name=function(str){
+//     console.log(str);
+// }
+
+// name("Asif");
+
+
+function myFun(a, b, ...manyMoreArgs) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("manyMoreArgs", manyMoreArgs);
+  }
+  
+  myFun("one", "two", "three", "four", "five", "six","Seven","Heven","eight");

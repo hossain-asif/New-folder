3d0a451395e6832857012ac3a713e4e394441cbc
hossain-asif <minhazhossainasif@gmail.com>
2024-01-18 20:13:04 +0600

    6

--- module-8/index.js
+++ module-8/index.js
@@ -1,11 +1,36 @@
 
 
-//5
+// 5
+// const http =require('http');
 
+// let server = http.createServer((req,res)=>{
+//     res.end('Minhaz Hossain Asif');
+// });
+
+// server.listen(5050);
+
+// console.log('Server Run Success');
+
+
+//6
 const http =require('http');
 
 let server = http.createServer((req,res)=>{
-    res.end('Minhaz Hossain Asif');
+    if(req.url == '/'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>This is Home page</h1>');
+        res.end();
+    }
+    else if(req.url == '/about'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>This is about page</h1>');
+        res.end();
+    }
+    else if(req.url == '/contact'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>This is contact page</h1>');
+        res.end();
+    }
 });
 
 server.listen(5050);

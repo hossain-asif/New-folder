42b40d599485bad49466e15b7e1e8c46a539a1a1
hossain-asif <minhazhossainasif@gmail.com>
2024-03-07 12:21:41 +0600

    ....

---
+++ module-13/task_management_system/app.js
@@ -0,0 +1,49 @@
+
+
+const express = require('express');
+const router = require('./src/route/api');
+const app = new express();
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Cors Open
+app.use(cors());
+
+//Security Implementation
+app.use(helmet());
+app.use(hpp());
+app.use(express.json({limit:'20mb'}));
+app.use(express.urlencoded({extended:true}));
+
+let limiter = rateLimit({windowMs:15*60*1000,max:3000});
+app.use(limiter);
+
+
+
+//Database connection
+let URL = "mongodb://localhost:27017/task-management";
+let OPTIONS = { autoIndex: true }; // Omit user and pass if not using authentication
+
+mongoose.connect(URL, OPTIONS)
+    .then(() => {
+        console.log("Database connection established");
+    })
+    .catch((err) => {
+        console.error("Connection error:", err);
+    });
+
+
+
+
+//Route Implement
+app.use("/api",router);
+
+app.use("*",(req,res)=>{
+    res.status(404).json({data:"Not Found"});
+});
+
+
+module.exports = app;
---
+++ module-13/task_management_system/index.js
@@ -0,0 +1,6 @@
+
+const app = require("./app");
+
+app.listen(300,()=>{
+    console.log("server running.....@3000 port");
+})
